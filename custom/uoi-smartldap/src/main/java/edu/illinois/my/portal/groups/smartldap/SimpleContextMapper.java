f2c9c66e45a964b367393dd9894e7865fd862cb7
Benito J. Gonzalez <bgonzalez@unicon.net>
2020-04-03 11:20:35 -0700

    fix(SimpleContextMapper): fix use of LdapName

--- custom/uoi-smartldap/src/main/java/edu/illinois/my/portal/groups/smartldap/SimpleContextMapper.java
+++ custom/uoi-smartldap/src/main/java/edu/illinois/my/portal/groups/smartldap/SimpleContextMapper.java
@@ -6,9 +6,9 @@
  * Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License.  You may obtain a
  * copy of the License at the following location:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,11 +18,6 @@
  */
 
 package edu.illinois.my.portal.groups.smartldap;
-
-import java.util.Enumeration;
-import java.util.LinkedList;
-import java.util.List;
-import javax.naming.directory.Attribute;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -34,26 +29,31 @@ import org.springframework.ldap.core.ContextMapper;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.DistinguishedName;
 
+import javax.naming.directory.Attribute;
+import javax.naming.ldap.LdapName;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+
 public final class SimpleContextMapper implements ContextMapper {
 
-    private static final String GROUP_DESCRIPTION =
-            "This group was pulled from the directory server.";
+    private static final String GROUP_DESCRIPTION = "This group was pulled from the directory server.";
+    private final Log log = LogFactory.getLog(getClass());
     /**
-     * Indicator on converting RDN keys to uppercase 
+     * Indicator on converting RDN keys to uppercase
      * normally true for active directory and false otherwise
      */
     private boolean convertRdnKeysToUpperCase = false;
     /**
-     * Name of the LDAP attribute on a group that tells you 
+     * Name of the LDAP attribute on a group that tells you
      * the name of the group.
      */
     private String groupNameAttributeName = null;
     /**
-     * Name of the LDAP attribute on a group that tells you 
+     * Name of the LDAP attribute on a group that tells you
      * who its children are.
      */
     private String membershipAttributeName = null;
-    private final Log log = LogFactory.getLog(getClass());
 
     /*
      * Public API.
@@ -79,70 +79,64 @@ public final class SimpleContextMapper implements ContextMapper {
         try {
             DirContextAdapter context = (DirContextAdapter) ctx;
 
-            DistinguishedName groupKeyDn = (DistinguishedName) context.getDn();
+            LdapName groupKeyDn = (LdapName) context.getDn();
             StringBuilder dnBuilder = new StringBuilder();
-            for (int i=groupKeyDn.size()-1;i>=0; i--) {
+            for (int i = groupKeyDn.size() - 1; i >= 0; i--) {
                 if (convertRdnKeysToUpperCase) {
-                    dnBuilder.append(groupKeyDn.getLdapRdn(i).getKey().toUpperCase());
+                    dnBuilder.append(groupKeyDn.getRdn(i).getType().toUpperCase());
                 } else {
-                    dnBuilder.append(groupKeyDn.getLdapRdn(i).getKey());
+                    dnBuilder.append(groupKeyDn.getRdn(i).getType());
                 }
-                dnBuilder.append('=').append(groupKeyDn.getLdapRdn(i).getValue());
-                if (i !=0) {
+                dnBuilder.append('=').append(groupKeyDn.getRdn(i).getValue());
+                if (i != 0) {
                     dnBuilder.append(',');
                 }
             }
-        String key = dnBuilder.toString();
-        if (log.isDebugEnabled()) {
-        log.debug("SmartLdap ContextMapper DN: " + key);
-        }
-        String groupName = (String) context.getStringAttribute(groupNameAttributeName);
-
-        IEntityGroup g = new EntityTestingGroupImpl(key, IPerson.class);
-        g.setCreatorID("System");
-        g.setName(groupName);
-        g.setDescription(GROUP_DESCRIPTION);
-        List<String> membership = new LinkedList<String>();
-        Attribute m = context.getAttributes().get(membershipAttributeName);
-        if (m != null) {
-            for (Enumeration<?> en = m.getAll(); en.hasMoreElements();) {
-                membership.add((String) en.nextElement());
+            String key = dnBuilder.toString();
+            if (log.isDebugEnabled()) {
+                log.debug("SmartLdap ContextMapper DN: " + key);
             }
-        }
-        rslt = new LdapRecord(g, membership);
+            String groupName = (String) context.getStringAttribute(groupNameAttributeName);
 
-        if (log.isDebugEnabled()) {
-            StringBuilder msg = new StringBuilder();
-            msg.append("Record Details:").append("\n\tkey=").append(key).append("\n\tgroupName=").append(groupName).append("\n\tmembers:");
-            for (String s : membership) {
-                msg.append("\n\t\t").append(s);
+            IEntityGroup g = new EntityTestingGroupImpl(key, IPerson.class);
+            g.setCreatorID("System");
+            g.setName(groupName);
+            g.setDescription(GROUP_DESCRIPTION);
+            List<String> membership = new LinkedList<String>();
+            Attribute m = context.getAttributes().get(membershipAttributeName);
+            if (m != null) {
+                for (Enumeration<?> en = m.getAll(); en.hasMoreElements(); ) {
+                    membership.add((String) en.nextElement());
+                }
             }
-            log.debug(msg.toString());
-        }
+            rslt = new LdapRecord(g, membership);
 
-    }
-    catch
-        (Throwable t) {
+            if (log.isDebugEnabled()) {
+                StringBuilder msg = new StringBuilder();
+                msg.append("Record Details:").append("\n\tkey=").append(key).append("\n\tgroupName=").append(groupName).append("\n\tmembers:");
+                for (String s : membership) {
+                    msg.append("\n\t\t").append(s);
+                }
+                log.debug(msg.toString());
+            }
+
+        } catch (Throwable t) {
             log.error("Error in SimpleContextMapper", t);
-        String msg = "SimpleContextMapper failed to create a LdapRecord from the specified Context: " + ctx;
-        throw new RuntimeException(msg, t);
+            String msg = "SimpleContextMapper failed to create a LdapRecord from the specified Context: " + ctx;
+            throw new RuntimeException(msg, t);
+        }
+        return rslt;
     }
-    return
-    rslt ;
-}
 
     public void setConvertRdnKeysToUpperCase(boolean convertRdnKeysToUpperCase) {
         this.convertRdnKeysToUpperCase = convertRdnKeysToUpperCase;
     }
-public void
 
-setGroupNameAttributeName(String groupNameAttributeName) {
+    public void setGroupNameAttributeName(String groupNameAttributeName) {
         this.groupNameAttributeName = groupNameAttributeName;
     }
 
-public void
-
-setMembershipAttributeName(String membershipAttributeName) {
+    public void setMembershipAttributeName(String membershipAttributeName) {
         this.membershipAttributeName = membershipAttributeName;
     }
 }
